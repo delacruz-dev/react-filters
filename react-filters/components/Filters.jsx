b4a248119bfe1dbb5d6398244733bb089bfe0830
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2015-02-18 14:00:36 +0100

    Changing price on selecting an building type element

--- react-filters/components/Filters.jsx
+++ react-filters/components/Filters.jsx
@@ -8,13 +8,18 @@ var PriceFilter = require('./PriceFilter')
 module.exports = React.createClass({
 	displayName: 'Filters',
 	getInitialState: function() {
+		var element = PRICES.filter(function(n){ return n.buildingType === 1; });
 		return {
 			buildingTypes: BUILDING_TYPES,
-			prices: PRICES
+			prices: element[0].prices
 		};
 	},
 	onBuildingTypeChange: function(){
-		console.log("The building type has changed");
+		console.log("changing prices");
+		var element = PRICES.filter(function(n){ return n.buildingType === 2; });
+		this.setState({
+			prices: element[0].prices 
+		});
 	},
 	render: function() {
 		return (
@@ -38,16 +43,28 @@ var BUILDING_TYPES = [
 	{ 'id' : 5, 'value' : 'Garages' },
 	{ 'id' : 6, 'value' : 'Land' },
 	{ 'id' : 7, 'value' : 'Offices' },
-	{ 'id' : 8, 'value' : 'Box rooms' },
+	{ 'id' : 8, 'value' : 'Box rooms' }
 ];
 
 
-var PRICES = [ 
-	{ 'id' : 150, 'value' : 'From 150' },
-	{ 'id' : 300, 'value' : 'From 300' },
-	{ 'id' : 450, 'value' : 'From 450' },
-	{ 'id' : 600, 'value' : 'From 600' },
-	{ 'id' : 700, 'value' : 'From 700' },
-	{ 'id' : 800, 'value' : 'From 800' },
-	{ 'id' : 900, 'value' : 'From 900' }
+var PRICES = [ { buildingType: 1, prices: [
+					{ 'id' : 150, 'value' : 'From 150' },
+					{ 'id' : 300, 'value' : 'From 300' },
+					{ 'id' : 450, 'value' : 'From 450' },
+					{ 'id' : 600, 'value' : 'From 600' },
+					{ 'id' : 700, 'value' : 'From 700' },
+					{ 'id' : 800, 'value' : 'From 800' },
+					{ 'id' : 900, 'value' : 'From 900' }
+					] 
+				},
+				{ buildingType: 2, prices: [
+					{ 'id' : 150000, 'value' : 'From 150.000' },
+					{ 'id' : 300000, 'value' : 'From 300.000' },
+					{ 'id' : 450000, 'value' : 'From 450.000' },
+					{ 'id' : 600000, 'value' : 'From 600.000' },
+					{ 'id' : 700000, 'value' : 'From 700.000' },
+					{ 'id' : 800000, 'value' : 'From 800.000' },
+					{ 'id' : 900000, 'value' : 'From 900.000' }
+					] 
+				}
 ];
