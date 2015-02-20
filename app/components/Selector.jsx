926182e6bcc261e733007f5aea37961964c8cb1f
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2015-02-20 16:10:24 +0100

    Modified forEach function with a fancy map approach

--- app/components/Selector.jsx
+++ app/components/Selector.jsx
@@ -7,13 +7,11 @@ var SelectItem = require('./SelectItem')
 module.exports = React.createClass({
 	displayName: 'Selector',
 	render: function() {
-		var selectItems = [];
 		var props = this.props;
-		props.selectItems.forEach(function(selectItem) {
-			selectItems.push(
-				<SelectItem id = {selectItem.id} 
-							value = {selectItem.value}  
-							onSelectItem = {props.onSelectItem} />);
+		var selectItems = props.selectItems.map(function(selectItem) {
+			return <SelectItem id = {selectItem.id} 
+							   value = {selectItem.value}  
+							   onSelectItem = {props.onSelectItem} />
 		});
 		return (
 			<ul>{selectItems}</ul>
